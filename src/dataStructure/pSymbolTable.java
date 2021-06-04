07390eeefd97465b727a55ea92ec0143d8337706
Gustavo Rigor <gusrigor@icloud.com>
2021-06-03 22:58:57 -0300

    add header e members

--- src/dataStructure/pSymbolTable.java
+++ src/dataStructure/pSymbolTable.java
@@ -1,5 +1,25 @@
 package dataStructure;
 
-public class pSymbolTable {
+import java.util.HashMap;
 
+public class pSymbolTable {
+	
+	private HashMap<String, pSymbol> map;
+	
+	public pSymbolTable() {
+		map = new HashMap<String, pSymbol>();
+	}
+	
+	public add(pSymbol symbol) {
+		map.put(symbol.getName(), symbol);
+	}
+	
+	public boolean exists(String symbolName) {
+		return map.get(symbolName) != null;
+	}
+	
+	public pSymbol get(String symbolName) {
+		return map.get(symbolName);
+	}
+	
 }
